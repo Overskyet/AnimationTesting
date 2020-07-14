8bc13ea0da6c0985ff42d15772f958a8009f4359
overskyet <paul.overskyet@gmail.com>
2020-07-14 15:48:08 +0300

    BottomSheet added

--- app/src/main/java/com/overskyet/animationtesting/BlankActivity.java
+++ app/src/main/java/com/overskyet/animationtesting/BlankActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.slidingpanelayout.widget.SlidingPaneLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
 public class BlankActivity extends AppCompatActivity {
 
@@ -14,6 +18,11 @@ public class BlankActivity extends AppCompatActivity {
 
         SlidingPaneLayout slidingPaneLayout = findViewById(R.id.sliding_pane);
         slidingPaneLayout.openPane();
+
+        FrameLayout frameLayout = findViewById(R.id.bottomSheet_frame_layout);
+//        BottomSheetBehavior.from(frameLayout).setHideable(true);
+        BottomSheetBehavior.from(frameLayout).setPeekHeight(50);
+        BottomSheetBehavior.from(frameLayout).setState(BottomSheetBehavior.STATE_EXPANDED);
     }
 
     @Override
